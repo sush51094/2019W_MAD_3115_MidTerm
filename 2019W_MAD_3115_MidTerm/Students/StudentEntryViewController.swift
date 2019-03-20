f9efa9384eb8c9eccb8b633136bbe76f88dd108c
MacStudent <macstudent@126-iMac12.local>
2019-03-20 19:04:23 -0400

    added info to studentEntryview controller

--- 2019W_MAD_3115_MidTerm/Students/StudentEntryViewController.swift
+++ 2019W_MAD_3115_MidTerm/Students/StudentEntryViewController.swift
@@ -8,15 +8,176 @@
 
 import UIKit
 
-class StudentEntryViewController: UIViewController {
-
+class StudentEntryViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+    
+    
+    
+    @IBOutlet weak var sid: UITextField!
+    
+    
+    @IBOutlet weak var txtsname: UITextField!
+    
+    
+    
+    @IBOutlet weak var btngender: UISegmentedControl!
+    
+    var bdate1 : String?
+    var course : String = ""
+    var gender: String?
+  //  var email : String?
+    
+    
+    
+    @IBOutlet weak var bdate: UIDatePicker!
+    
+    
+    @IBOutlet weak var email: UITextField!
+    
+    
+    
+    @IBOutlet weak var coursepicker: UIPickerView!
+    
+    @IBOutlet weak var m1: UITextField!
+    
+    @IBOutlet weak var lblentry: UILabel!
+    
+    
+    
+    @IBOutlet weak var m2: UITextField!
+    
+    @IBOutlet weak var m3: UITextField!
+    
+    
+    @IBOutlet weak var m4: UITextField!
+    
+    
+    @IBOutlet weak var m5: UITextField!
+    
+    
+    var pickerDataFirstName = ["MADT", "MODT", "CSD", "WADT"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.coursepicker.delegate = self
+        self.coursepicker.dataSource = self
 
         // Do any additional setup after loading the view.
     }
     
+    
+    
+    
+    @IBAction func gender(_ sender: UISegmentedControl) {
+        
+        if(sender.selectedSegmentIndex==0)
+        {
+            gender = "Male"
+        }
+        
+        else
+        {
+            gender = "Female"
+        }
+        
+        
+        
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            
+            return 2
+        }
+        
+        
+    }
+    
+    
+    
+    
+    @IBAction func picker1date(_ sender: UIDatePicker) {
+        
+        let dateFormatter = DateFormatter()
+        
+        dateFormatter.dateFormat = "dd-mm-yyy"
+        
+        let strDate = dateFormatter.string(from: sender.date)
+        
+        bdate1 = strDate
+        
+        
+    }
+    
+    
+    
+    
+    
+    @IBAction func cal(_ sender: Any) {
+        
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let homevc = sb.instantiateViewController(withIdentifier: "studentresul") as! StudentResultViewController
+        homevc.sid = sid.text
+        homevc.sname = txtsname.text
+        homevc.gender2 = gender
+        homevc.course1 = course
+        homevc.bdate1 = bdate1
+        homevc.email1 = email.text
+        homevc.m1 = Int(m1.text!)
+        homevc.m2 = Int(m2.text!)
+        homevc.m3 = Int(m3.text!)
+        homevc.m4 = Int(m4.text!)
+        homevc.m5 = Int(m5.text!)
+        
+        self.navigationController?.pushViewController(homevc, animated: true)
+        
+        
 
+    }
+    
+    
+    
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        if component == 0
+        {
+            
+        }
+        
+        else
+        {
+            
+            return pickerDataFirstName.count
+        }
+        
+      return 0
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        if component == 1
+        
+        {
+            
+            return pickerDataFirstName[row]
+        
+        
+        
+        
+    }
+        
+        return nil
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
--- 2019W_MAD_3115_MidTerm/Students/StudentResultViewController.swift
+++ 2019W_MAD_3115_MidTerm/Students/StudentResultViewController.swift
@@ -9,7 +9,67 @@
 import UIKit
 
 class StudentResultViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var txtsid: UILabel!
+    
+    
+    @IBOutlet weak var studentname: UILabel!
+    
+    
+    @IBOutlet weak var gender: UILabel!
+    
+    
+    
+    @IBOutlet weak var birthdate: UILabel!
+    
+    
+    @IBOutlet weak var email: UILabel!
+    
+    
+    @IBOutlet weak var course: UILabel!
+    
+    
+    @IBOutlet weak var marks1: UILabel!
+    
+    
+    @IBOutlet weak var marks2: UILabel!
+    
+    
+    @IBOutlet weak var marks3: UILabel!
+    
+    
+    
+    @IBOutlet weak var marks4: UILabel!
+    
+    
+    @IBOutlet weak var total: UILabel!
+    
+    
+    @IBOutlet weak var per: UILabel!
+    
+    
+    @IBOutlet weak var grade1: UILabel!
+    
+    var sid : String?
+    var sname : String?
+    var gender2 : String?
+    var bdate1 : String?
+    var course1 : String?
+    var email1 : String?
+    var m1,m2,m3,m4,m5 : Int?
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
