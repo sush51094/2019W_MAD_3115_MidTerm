2e7d6a0b806cc5736cbd3eca6140e895b50668a4
MacStudent <macstudent@126-iMac12.local>
2019-03-20 18:28:22 -0400

    added login screen

--- 2019W_MAD_3115_MidTerm/LoginViewController.swift
+++ 2019W_MAD_3115_MidTerm/LoginViewController.swift
@@ -9,12 +9,68 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var lbllogin: UILabel!
+    
+    
+    @IBOutlet weak var lblemail: UITextField!
+    
+    
+    @IBOutlet weak var txtpassword: UITextField!
+    
 
+    @IBOutlet weak var lblrememberMe: UILabel!
+    
+    
+    
+    @IBOutlet weak var switchstatus: UISwitch!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func btnLoginClick(_ sender: UIButton) {
+        
+        if(lblemail.text == "admin" && txtpassword.text == "admin@123")
+        {
+            let sb = UIStoryboard(name: "Main", bundle: nil)
+            let homevc = sb.instantiateViewController(withIdentifier: "studentinfo") as! StudentEntryViewController
+            self.navigationController?.pushViewController(homevc, animated: true)
+            
+            
+            
+        }
+        
+        else
+        {
+            
+            
+let Alert = UIAlertController(title: nil, message: "OK", preferredStyle: .alert)
+            let addaction = UIAlertAction(title: "Inavlid", style: .default, handler: nil)
+            
+            Alert.addAction(addaction)
+            self.present(Alert,animated: true)
+            
+            self.lblemail.text = ""
+            self.txtpassword.text = ""
+            
+            
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+    
 }
 
+}
